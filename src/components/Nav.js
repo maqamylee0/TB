f927400c10e04f91230da82d017040c4b12fa9cc
maqamylee0 <maqamylee0@gmail.com>
2022-10-25 22:35:00 +0300

    signup

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import './Nav.css';
+     function Nav() {
+            return (
+                <nav className="main-nav">
+                    <ul>
+                        <li>Home</li>
+                        <li>Articles</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            );
+        }
+        
+    
+
+export default Nav;
---
+++ src/components/Register.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
+
+function Register() {
+  return (
+    <MDBContainer fluid>
+
+      <MDBCard className='text-black m-5' style={{borderRadius: '25px'}}>
+        <MDBCardBody>
+          <MDBRow>
+            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+
+              <p classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign In</p>
+
+              <div className="d-flex flex-row align-items-center mb-4 ">
+                <MDBIcon fas icon="user me-3" size='lg'/>
+                <MDBInput label='Your Name' id='form1' type='text' className='w-100'/>
+              </div>
+
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MDBIcon fas icon="lock me-3" size='lg'/>
+                <MDBInput label='NIN' id='form3' type='text'/>
+              </div>
+
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MDBIcon fas icon="lock me-3" size='lg'/>
+                <MDBInput label='Password' id='form3' type='password'/>
+              </div>
+
+             
+
+              <div className='mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              </div>
+
+              <MDBBtn className='mb-4' size='lg'>Sign In</MDBBtn>
+
+            </MDBCol>
+
+            <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+              <MDBCardImage src="require('@/assets/cover.jpg')"alt="Card image cap" />
+
+            </MDBCol>
+
+          </MDBRow>
+        </MDBCardBody>
+      </MDBCard>
+
+    </MDBContainer>
+  );
+}
+
+export default Register;
